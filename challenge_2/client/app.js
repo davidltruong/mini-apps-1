6692e08064def94a6e93d8382676891d71381dab
David Truong <dtruong1992@gmail.com>
2022-05-21 15:24:10 -0700

    Complete CSV-Report-Generator Advanced Content

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,34 @@
+$(document).ready(function () {
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    var data = new FormData();
+    var file = e.target[0].files[0]
+    data.append('file', file)
 
+    $.ajax({
+      method: "POST",
+      url: "/",
+      data: data,
+      processData: false,
+      contentType: false,
+      success: function(data) {
+        $('#csv').html('<br>' + data)
+        $('body').append('<br>')
+        $('body').append('<form id=download method="GET" action="/download"></form>')
+        $('#download').append('<input id="submit" type="submit" value="Download"></input>')
+      },
+      error: function() {console.log('error')}
+    })
+  });
+});
 
+var download = () => {
+  $.ajax({
+    method: "GET",
+    url: "/",
+    success: function(file) {
+      console.log(file)
+
+    }
+  })
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,72 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var port = 8888;
+var bodyParser = require('body-parser')
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var path = require('path');
+
+app.set('view engine', 'pug')
+app.use(express.static('client'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//function to handle data and convert to csv /////////////////////////////////////////////////////
+var csv = function (data) {
+  var result = 'firstName,lastName,county,city,role,sales<br>'
+  var dataHandler = function (obj) {
+    var firstName = obj.firstName;
+    var lastName = obj.lastName;
+    var county = obj.county;
+    var city = obj.city;
+    var role = obj.role;
+    var sales = obj.sales;
+    result += firstName + ',' + lastName + ',' + county
+    + ',' + city + ',' + role + ',' + sales + '<br>';
+    if (obj.children) {
+      for (var i = 0; i < obj.children.length; i++) {
+        dataHandler(obj.children[i])
+      }
+    }
+  }
+  dataHandler(data);
+  return result;
+}
+
+
+//handle post request for submit button //////////////////////////////////////////////////////////
+
+var filePath = path.join(__dirname, '/download.txt');
+
+app.get('/download', (req, res) => {
+  console.log('download')
+  res.download('download.txt')
+})
+
+app.post('/', upload.single('file'), (req, res) => {
+  var path = req.file.path;
+  fs.readFile( './' + path , 'utf8', (err, data) => {
+    var file = csv(JSON.parse(data))
+    fs.unlink('./' + path, (err, data) => {
+      var download = file.split('<br>').join('\n')
+      fs.writeFile(filePath, download, function(err, data) {
+        res.send(file)
+      })
+    })
+  })
+})
+
+app.post('/upload_json', upload.single('data'), (req, res) => {
+  var path = req.file.path;
+  fs.readFile( './' + path , 'utf8', (err, data) => {
+    var file = csv(JSON.parse(data))
+    fs.unlink('./' + path, (err, data) => {
+      res.render('upload_json', {div: file})
+    })
+  })
+})
+
+app.listen(port, function() {
+  console.log(`App listening on port ${port}!`)
+});
