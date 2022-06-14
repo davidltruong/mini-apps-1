65fe6f9e78dbc6df83161492b2d69d54cf8155aa
David Truong <dtruong1992@gmail.com>
2022-06-13 20:30:08 -0700

    progress

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,16 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
 
 
+
+  render () {
+    return (<button onClick={(e) => {console.log(e)}}>checkout</button>)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
